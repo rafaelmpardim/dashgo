8b4c3462ceff3f934f31e3cdd8433c242385063a
rafaelmpardim <rafaelmedeirospardim@gmail.com>
2022-10-21 15:11:17 -0300

    feat: creating user data hook

---
+++ src/services/hooks/useUsers.ts
@@ -0,0 +1,49 @@
+import { useQuery } from 'react-query'
+
+import { api } from '../axios/api'
+
+type User = {
+	createdAt: string
+	email: string
+	id: string
+	name: string
+}
+
+type GetUsersResponse = {
+	users: User[],
+	totalCount: number
+}
+
+export async function getUsers(page: number): Promise<GetUsersResponse> {
+	const { data, headers } = await api.get('http://localhost:3000/api/users', {
+		params: {
+			page: page
+		}
+	})
+
+	const totalCount = Number(headers['x-total-count'])
+
+	const users = data.users.map(user => {
+		return {
+			createdAt: new Date(user.createdAt).toLocaleDateString('pt-BR', {
+				day: '2-digit',
+				month: 'long',
+				year: 'numeric'
+			}),
+			email: user.email,
+			id: user.id,
+			name: user.name,
+		}
+	})
+
+	return {
+		users,
+		totalCount
+	}
+}
+
+export function useUsers(page: number) {
+	return useQuery(['users', page], () => getUsers(page), {
+		staleTime: 10 * 60 * 1000
+	})
+}
