e2e606bd4b6bf7e2ed96384f9782c44f1d4ddcf2
rafaelmpardim <rafaelmedeirospardim@gmail.com>
2022-10-21 15:07:16 -0300

    feat: implementing QueryClient and QueryDevTools

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,17 +1,33 @@
 import { React } from 'react-dom'
+import { QueryClientProvider } from 'react-query'
+import { ReactQueryDevtools } from 'react-query/devtools'
+
 import { AppProps } from 'next/app'
 
+import { makeServer } from '../services/mirage'
+import { queryClient } from '../services/queryClient'
+
 import { ChakraProvider } from '@chakra-ui/react'
-import { theme } from '../styles/theme'
+
 import { SidebarDrawerProvider } from '../context/SidebarDrawerContext'
+
+import { theme } from '../styles/theme'
+
+if (process.env.NODE_ENV == 'development') {
+	makeServer()
+}
 
 function MyApp({ Component, pageProps }: AppProps) {
 	return (
-		<ChakraProvider theme={theme}>
-			<SidebarDrawerProvider>
-				<Component {...pageProps} />
-			</SidebarDrawerProvider>
-		</ChakraProvider>
+		<QueryClientProvider client={queryClient}>
+			<ChakraProvider theme={theme}>
+				<SidebarDrawerProvider>
+					<Component {...pageProps} />
+				</SidebarDrawerProvider>
+			</ChakraProvider>
+			
+			<ReactQueryDevtools />
+		</QueryClientProvider>
 	)
 }
 
