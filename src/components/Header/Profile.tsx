20fbe9a770c8085d56f3b43fc180f5d96355ace0
rafaelmpardim <rafaelmedeirospardim@gmail.com>
2022-10-27 00:43:06 -0300

    refactor: rendering Profile user data

--- src/components/Header/Profile.tsx
+++ src/components/Header/Profile.tsx
@@ -1,25 +1,38 @@
-import { Flex, Avatar, Text, Box } from '@chakra-ui/react'
+import { Button, Flex, Avatar, Text, Box, Icon } from '@chakra-ui/react'
+
+import { AuthContext, signOut } from '../../context/AuthContext'
+
+import { FiLogOut } from 'react-icons/fi'
+import { useContext } from 'react'
 
 interface ProfileProps {
 	showProfileData?: boolean
 }
 
 export function Profile({ showProfileData = true}: ProfileProps) {
+	const { user } = useContext(AuthContext)
+
 	return (
 		<Flex align='center' >
 			{ showProfileData && (
 				<Box mr='4' textAlign='right'>
-					<Text>Rafael Pardim</Text>
-					<Text color='gray.300' fontSize='sm'>rafael@madeincode.com.br</Text>
+					<Text>{user?.name}</Text>
+					<Text color='gray.300' fontSize='sm'>{user?.email}</Text>
 				</Box>
 			)}
 			<Avatar
 			  bg='pink.500'
 			  color='gray.50'
-			  name='Rafael Pardim'
+			  name={user?.name}
 			  size='md'
-			  src='https://github.com/rafaelmpardim.png'
 			/>
+			<Button
+				colorScheme='unstyled'
+				onClick={signOut}
+				type='button'
+			>
+				<Icon as={FiLogOut} fontSize='20' />
+			</Button>
 		</Flex>
 	)
 }
