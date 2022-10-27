1d5e5a735009ce4ac68fe61e6fd8e10210e46cad
rafaelmpardim <rafaelmedeirospardim@gmail.com>
2022-10-27 00:44:48 -0300

    refactor: implementing permissions

--- src/components/Sidebar/SidebarNav.tsx
+++ src/components/Sidebar/SidebarNav.tsx
@@ -3,19 +3,36 @@ import { NavLink } from './NavLink'
 import { NavSection } from './NavSection'
 
 import { RiDashboardLine, RiContactsLine, RiInputMethodLine, RiGitMergeLine } from 'react-icons/ri'
+import { AuthContext } from '../../context/AuthContext'
+import { useContext } from 'react'
+import { Can } from '../Can'
 
 export function SidebarNav() {
+	const { user } = useContext(AuthContext)
+
 	return (
 		<Stack align='flex-start' spacing='8'>
 			<NavSection title='GERAL'>
 				<NavLink href='/dashboard' icon={RiDashboardLine}>Dashboard</NavLink>
-				<NavLink href='/users' icon={RiContactsLine}>Usuários</NavLink>
+				<Can
+					roles={['admin']}
+				>
+					<NavLink href='/users' icon={RiContactsLine}>Usuários</NavLink>
+				</Can>
 			</NavSection>
 
-			<NavSection title='AUTOMAÇÃO'>
-				<NavLink href='/forms' icon={RiInputMethodLine}>Formulários</NavLink>
-				<NavLink href='/automation' icon={RiGitMergeLine}>Automação</NavLink>
-			</NavSection>
+			<Can
+				roles={['admin']}
+			>
+				<NavSection title='AUTOMAÇÃO'>
+					<Can
+						permissions={['admin.master']}
+					>
+						<NavLink href='/forms' icon={RiInputMethodLine}>Formulários</NavLink>
+					</Can>
+					<NavLink href='/automation' icon={RiGitMergeLine}>Automação</NavLink>
+				</NavSection>
+			</Can>
 		</Stack>
 	)
 }
